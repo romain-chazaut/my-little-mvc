a21d04a886e90d34cda84a2424fd190ba36553ba
Augustin <augustin.yvon@laplateforme.io>
2024-01-30 11:34:58 +0100

    job-02

--- shop.php
+++ shop.php
@@ -4,8 +4,10 @@ require_once 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$product = new \App\Model\Clothing();
+$cloth = new \App\Model\Clothing();
+$electronic = new \App\Model\Electronic();
+$products = array_merge($cloth->findAll(), $electronic->findAll());
 
-var_dump($product->findAll());
+var_dump($products);
 
 
